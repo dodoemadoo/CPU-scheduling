9d741dd162679b173f10d9a8353a3e700e36ac0c
dodoemadoo <36114079+dodoemadoo@users.noreply.github.com>
2019-12-01 21:51:22 +0200

    Update shortestJobFirst.java

--- shortestJobFirst.java
+++ shortestJobFirst.java
@@ -6,14 +6,15 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-class The_Comparator implements Comparator<Process> { 
+class The_Comparator implements Comparator<Process> 
+{ 
     public int compare(Process p1, Process p2) 
     { 
-        if(p1.ArrivalTime>p2.ArrivalTime)
+        if(p1.arrivalTime>p2.arrivalTime)
         {
         return 1;
         }
-        else if(p1.ArrivalTime<p2.ArrivalTime){
+        else if(p1.arrivalTime<p2.arrivalTime){
         return -1;
         }
         return 0;
@@ -21,43 +22,40 @@ class The_Comparator implements Comparator<Process> {
 }
 class Process
 {
-	String ProcessName;
-	String ProcessColor;
-	int ArrivalTime;
-	int BurstTime;
+	String processName;
+	String processColor;
+	int arrivalTime;
+	int burstTime;
 	int waitingTime;
 	int tournarTime;
 	boolean visited;
 }
 
-public class shortestJobFirst {
+public class shortestJobFirst 
+{
     PriorityQueue<Process> queue;
-    shortestJobFirst(){
-    Scanner s=new Scanner(System.in); 
-    int numofprocess,At,Bt;  
-    String name,colour;
-    System.out.println("Enter the number of Processes :");
-    numofprocess=s.nextInt();
-    PriorityQueue<Process> queue = new PriorityQueue<Process>(numofprocess,new The_Comparator());
-    for(int i=0;i<numofprocess;i++){
-    Process p=new Process();
-    name=s.nextLine(); 
-    System.out.println("Enter the name :");
-    name=s.nextLine();  
-    System.out.println("Enter the color :");
-    colour=s.nextLine();
-    System.out.println("Enter the Arrival time :");
-    At=s.nextInt();
-    System.out.println("Enter the Burst Time :");
-    Bt=s.nextInt();
-    p.ArrivalTime=At;
-    p.BurstTime=Bt;
-    p.ProcessName=name;
-    p.ProcessColor=colour;
-    p.visited=false;
-    queue.add(p);
-    }      
-List<Integer>res=makechart(queue);
+	private Scanner scan= new Scanner(System.in);
+	private int numOfProcess;
+    shortestJobFirst()
+    {
+	    System.out.print("Enter the number of processes: ");
+	    numOfProcess=scan.nextInt();
+	    PriorityQueue<Process> queue = new PriorityQueue<Process>(numOfProcess,new The_Comparator());
+	    for(int i=0;i<numOfProcess;i++)
+	    {
+		    Process p=new Process();
+		    System.out.print("Enter the name: ");
+		    p.processName=scan.nextLine();  
+		    System.out.print("Enter the color: ");
+		    p.processColor=scan.nextLine();
+		    System.out.print("Enter the Arrival time: ");
+		    p.arrivalTime=scan.nextInt();
+		    System.out.print("Enter the Burst Time: ");
+		    p.burstTime=scan.nextInt();
+		    p.visited=false;
+		    queue.add(p);
+	    }      
+	    List<Integer>res=makechart(queue);
         System.out.println("The Process order is..");
         Iterator value = queue.iterator();
         int avWaiting=0,avtounar=0;
@@ -65,10 +63,10 @@ List<Integer>res=makechart(queue);
            while(value.hasNext()){
                Process nex=(Process) value.next(); 
               if(i!=res.size()-1){ 
-              if(res.get(i+1)-res.get(i)==nex.BurstTime && nex.visited==false){
-                  nex.waitingTime=res.get(i)-nex.ArrivalTime;
-                  nex.tournarTime=nex.waitingTime+nex.BurstTime;
-                  System.out.println(nex.ProcessName+" "+nex.waitingTime+" "+nex.tournarTime);
+              if(res.get(i+1)-res.get(i)==nex.burstTime && nex.visited==false){
+                  nex.waitingTime=res.get(i)-nex.arrivalTime;
+                  nex.tournarTime=nex.waitingTime+nex.burstTime;
+                  System.out.println(nex.processName+" "+nex.waitingTime+" "+nex.tournarTime);
                   nex.visited=true;
                   avWaiting+=nex.waitingTime;
                   avtounar+=nex.tournarTime;
@@ -92,9 +90,9 @@ Iterator value = q.iterator();
      Process nex=(Process) value.next();
        for(int j=0;j<2;j++){
            if(j==0)
-           temp[i][j]=nex.ArrivalTime;
+           temp[i][j]=nex.arrivalTime;
            else
-             temp[i][j]=nex.BurstTime;           
+             temp[i][j]=nex.burstTime;           
  }
  }
  if(all(temp)){
