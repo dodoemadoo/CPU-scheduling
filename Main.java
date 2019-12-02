aa0448214d8d65be5483cd69210ec8cc1ad61bd4
dodoemadoo <36114079+dodoemadoo@users.noreply.github.com>
2019-12-02 15:58:01 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,33 +6,57 @@ public class Main
 	public static String choice;
 	private static shortestJobFirst sjf;
 	private static shortestRemainingTimeFirst srtf;
-	private static AG ag;
+	private static AG agScheduling;
 	private static priorityScheduling pScheduling;
 	public static void main(String[] args) 
 	{
 		System.out.println("Enter (1) to make shortest job first scheduling.");
 		System.out.println("Enter (2) to make shortest remaining time first scheduling.");
 		System.out.println("Enter (2) to make priority scheduling.");
-		System.out.println("Enter (4) to make AG scheduling.");
+		System.out.println("Enter (4) to make ag scheduling.");
 		System.out.print("Enter your choice: ");
 		choice = scan.nextLine();
 		switch (choice) {
 		case "1":
-			sjf = new shortestJobFirst();
+			setSjf(new shortestJobFirst());
 			break;
 		case "2":
-			srtf = new shortestRemainingTimeFirst();
+			setSrtf(new shortestRemainingTimeFirst());
 			break;
 		case "3":
-			pScheduling = new priorityScheduling();
+			setpScheduling(new priorityScheduling());
 			break;
 		case "4":
-			ag = new AG();
+			setAgScheduling(new AG());
 			break;
 		default:
 			System.out.println("Invalid input..!");
 			break;
 		}
+	}
+	public static shortestJobFirst getSjf() {
+		return sjf;
+	}
+	public static void setSjf(shortestJobFirst sjf) {
+		Main.sjf = sjf;
+	}
+	public static shortestRemainingTimeFirst getSrtf() {
+		return srtf;
+	}
+	public static void setSrtf(shortestRemainingTimeFirst srtf) {
+		Main.srtf = srtf;
+	}
+	public static AG getAgScheduling() {
+		return agScheduling;
+	}
+	public static void setAgScheduling(AG agScheduling) {
+		Main.agScheduling = agScheduling;
+	}
+	public static priorityScheduling getpScheduling() {
+		return pScheduling;
+	}
+	public static void setpScheduling(priorityScheduling pScheduling) {
+		Main.pScheduling = pScheduling;
 	}
 
 }
